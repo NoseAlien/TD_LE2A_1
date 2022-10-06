c8441e2abb5a210b0857d661f51497b48766e4cd
ソンカイヨウ <k021g1268@g.neec.ac.jp>
2022-10-06 15:19:14 +0900

    Test

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	dxCommon = DirectXCommon::GetInstance();
 	dxCommon->Initialize(win);
 
+
 #pragma region 汎用機能初期化
 	// 入力の初期化
 	input = Input::GetInstance();
