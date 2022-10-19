040e1c9d1f7ea50b7a446bd64626d4ad65cf9b07
NoseAlien <k021g1117@g.neec.ac.jp>
2022-10-19 13:05:11 +0900

    欠片をつぶしたときのパーティクル改良

--- GrainScatterEffect.cpp
+++ GrainScatterEffect.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 GrainScatterEffect::GrainScatterEffect() :
-	maxParticle(8)
+	maxParticle(7)
 {
 }
 
@@ -16,15 +16,18 @@ GrainScatterEffect::~GrainScatterEffect()
 
 void GrainScatterEffect::Generate(const Vector3& pos)
 {
+	float startRad = DegreeToRad(Random::Range(0, 360 / maxParticle));
 	for (int i = 0; i < maxParticle; i++)
 	{
-		float radian = DegreeToRad(360 / maxParticle * i);
+		float radian = startRad + DegreeToRad(360 / maxParticle * i);
+
+		float particleScale = Random::RangeF(0.6, 0.2);
 
 		particles.emplace_back(move(make_unique<Particle>(1)));
 		particles.back()->SetPos(pos);
-		particles.back()->SetSpeed(0.5);
+		particles.back()->SetSpeed(0.7);
 		particles.back()->SetVec(Vector3(cosf(radian), sinf(radian), 1).Normalized());
-		particles.back()->SetScale({ 0.5,0.5,0.5 });
+		particles.back()->SetScale({ particleScale,particleScale,particleScale });
 		particles.back()->SetSpriteColor({ 0.248,0.839,0.396,1 });
 	}
 }
@@ -36,13 +39,13 @@ void GrainScatterEffect::Update()
 	for (int i = 0; i < particles.size(); i++)
 	{
 		auto tempScale = particles[i]->GetScale();
-		tempScale -= 0.02;
+		tempScale -= 0.015;
 		if (tempScale.x <= 0) tempScale = { 0,0,0 };
 		particles[i]->SetScale(tempScale);
 
 		particles[i]->SetSpriteSize({ 100 * tempScale.x,100 * tempScale.y });
 
-		particles[i]->SetSpeed(particles[i]->GetSpeed() * 0.95);
+		particles[i]->SetSpeed(particles[i]->GetSpeed() * 0.9);
 
 		particles[i]->UpdateSprite();
 	}
